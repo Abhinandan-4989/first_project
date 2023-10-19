895b4e0c7acba1c6d1447ea1afb8373aae0ff896
Abhinandan Wandhare <abhi_wandhare4989@yahoo.com>
2023-10-19 20:05:27 +0530

    Change in feature branch

--- food/models.py
+++ food/models.py
@@ -10,4 +10,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
-    item_images = models.CharField(max_length=900, default="https://cdn.vectorstock.com/i/1000x1000/37/07/fresh-vegetables-with-placeholder-for-text-vector-6263707.webp")
+    item_images = models.CharField(max_length=1, default="https://cdn.vectorstock.com/i/1000x1000/37/07/fresh-vegetables-with-placeholder-for-text-vector-6263707.webp")
